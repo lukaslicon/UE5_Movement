db56f73a48df5d5ba57f5c4dc103a002d6be562b
Lukas Licon <82681042+lukaslicon@users.noreply.github.com>
2024-08-31 20:02:12 -0700

    added run and idle animations with state machine

--- Source/ARPG_Movement/Private/Characters/ARPGAnimInstance.cpp
+++ Source/ARPG_Movement/Private/Characters/ARPGAnimInstance.cpp
@@ -1,5 +1,27 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Characters/ARPGAnimInstance.h"
+#include "Characters/ARPGCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
+void UARPGAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	ARPGCharacter = Cast<AARPGCharacter>(TryGetPawnOwner());
+	if (ARPGCharacter)
+	{
+		ARPGCharacterMovement = ARPGCharacter->GetCharacterMovement();
+	}
+}
+
+void UARPGAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (ARPGCharacterMovement)
+	{
+		GroundSpeed = UKismetMathLibrary::VSizeXY(ARPGCharacterMovement->Velocity);
+	}
+}
--- Source/ARPG_Movement/Public/Characters/ARPGAnimInstance.h
+++ Source/ARPG_Movement/Public/Characters/ARPGAnimInstance.h
@@ -1,17 +1,25 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
 #include "ARPGAnimInstance.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class ARPG_MOVEMENT_API UARPGAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	class AARPGCharacter* ARPGCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	class UCharacterMovementComponent* ARPGCharacterMovement;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	float GroundSpeed;
 };
