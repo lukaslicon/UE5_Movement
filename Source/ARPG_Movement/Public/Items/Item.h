56039c6d5e2d1af87d974f12a73b97d05c0906da
Lukas Licon <82681042+lukaslicon@users.noreply.github.com>
2024-09-02 14:14:09 -0700

    delegate testing for overlapping upgrade pickups

---
+++ Source/ARPG_Movement/Public/Items/Item.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class ARPG_MOVEMENT_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AItem();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Sine Parameters")
+	float Amplitude = 0.25f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Sine Parameters")
+	float TimeConstant = 0.5f;
+
+	UFUNCTION(BlueprintPure)
+	float TransformedSin();
+
+	UFUNCTION(BlueprintPure)
+	float TransformedCos();
+	 
+	template<typename T>
+	T Avg(T First, T Second);
+
+	UFUNCTION()
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float RunningTime;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* ItemMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* Sphere;
+};
+template<typename T>
+
+inline T AItem::Avg(T First, T Second) {
+	return (First + Second) / 2;
+}
